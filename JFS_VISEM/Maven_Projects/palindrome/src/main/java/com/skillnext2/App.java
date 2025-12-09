3a4480617d57b02a686809f17e08ae79a665c2d8
Manideep Ganji <ganjimanideep65@gmail.com>
2025-12-09 12:10:25 +0530

    Added

---
+++ JFS_VISEM/Maven_Projects/palindrome/src/main/java/com/skillnext2/App.java
@@ -0,0 +1,25 @@
+package com.skillnext2;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int reverse = 0;
+
+        while (num != 0) {
+            int rem = num % 10;              reverse = reverse * 10 + rem;             
+num = num / 10;        }
+
+        if (original == reverse)
+            System.out.println(original + " is a Palindrome.");
+        else
+            System.out.println(original + " is NOT a Palindrome.");
+    }
+}
+
